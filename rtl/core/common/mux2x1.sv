53597154c1cfe5e5dff79be9727f01abed682d0e
MuhammadHammad001 <hammad.bashir@10xengineers.ai>
2025-02-01 12:19:28 +0500

    Initial Commit

---
+++ rtl/core/common/mux2x1.sv
@@ -0,0 +1,14 @@
+module mux(inp1, inp2, out);
+    input   logic [31:0]    inp1,
+    input   logic [31:0]    inp2,
+    input   logic           sel ,
+    output  logic [31:0]    out
+
+    assign out = (sel) ? inp1 : inp2;
+    // always_comb begin
+    //     if (sel == 1)
+    //         out = inp1;
+    //     else
+    //         out = inp2;
+    // end
+endmodule
---
+++ verif/tb/internal_tbs/mux2x1_tb.sv
@@ -0,0 +1,46 @@
+module mux2x1_tb ();
+    //Declare signals
+    logic [31:0]    inp1,
+    logic [31:0]    inp2,
+    logic           sel ,
+    logic [31:0]    out
+
+    //Instancing mux module here
+    mux2x1 mux(
+        .inp1(inp1),
+        .inp2(inp2),
+        .sel(sel),
+        .out(out)
+    );
+
+    initial begin
+        inp1 = '0;
+        inp2 = '0;
+        sel  = '0;
+        #10;
+
+        $display("Output out = %d", out);
+
+        inp1 = 32'd10;
+        inp2 = 32'd20;
+        sel  = 1'b0;
+        #10;
+
+        $display("Output out = %d", out);
+
+        inp1 = 32'd10;
+        inp2 = 32'd20;
+        sel  = 1'b1;
+        #10;
+
+        $display("Output out = %d", out);
+
+        $stop
+    end
+
+    //dump all the inputs and outputs to a waveform
+    initial begin
+        $dumpvars();
+        $dumpfile("result.vcd");
+    end
+endmodule
